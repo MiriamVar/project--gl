57ac1388d0f004a496f594b4e320f2153c2cf419
Tomino98sv <tomas.pavlik@akademiasovy.sk>
2018-12-02 21:42:40 +0100

    rozdelenie na 2 hracov,vstupy mien,tabulky s menami hracov a ich uspechy

--- Pexeso/js/playPexeso.js
+++ Pexeso/js/playPexeso.js
@@ -1,3 +1,15 @@
+let playerOne = {
+				name: "",
+				turn:true,
+				point:0
+}
+let playerTwo = {
+				name: "",
+				turn:false,
+				point:0
+}
+
+
 // napevno nie
 let sizeX = 3;
 let sizeY = 4;
@@ -19,7 +31,7 @@ function createGameboard() {
 		}
 
 	}
-	console.log(gameboard);
+	//console.log(gameboard);
 }
 
 function shuffle() {					//FUNKCIA NA NAHODNE PREHADZANIE KARIET
@@ -67,8 +79,6 @@ let clicker = 0;
 let memoryname;
 let memoryid;
 
-let points = 0;
-
 function flipCard(id) {			//FUNKCIA OTOCI KARTU PO KLIKNUTI
 	for (let i = 0; i < gameboard.length; i++) {
 		if (gameboard[i].id === id) {
@@ -86,7 +96,7 @@ function resetFlip() {	//Prejde cez cele pole a vsetky karty oznaci ako false (p
 
 
 function repaint() {	// FUNKCIA, ZAVOLA SA NA PREKLESLENIE PEXESA
-	console.log('zobrazujem zmenene data (pole gameboard)');
+	//console.log('zobrazujem zmenene data (pole gameboard)');
 	for (let i = 0; i < gameboard.length; i++) {
 		if (gameboard[i].flipped == true) {
 			let x = document.getElementById(gameboard[i].id);
@@ -105,18 +115,21 @@ function resetVars() { // FUNKCOA, RESETUJE PREMENNE
 	countcardT = 0;
 	memoryname = null;
 	memoryid = null;
-}
+	playerOne.turn=!playerOne.turn;	//zneguje jeho hodnotu aby mohol ist dalsi hrac
+	playerTwo.turn=!playerTwo.turn;	//zneguje jeho hodnotu aby mohol ist dalsi hrac
 
+}
+let winner;
 function match(event) {
 
-	console.log('menim data (pole gameboard)');
+	//console.log('menim data (pole gameboard)');
 	if (clicker == 2) { 				//RESTART PO 2 KLIKOCH AKO KOLO
 		resetVars();
 		resetFlip();
 	}
 
 	flipCard(event.target.id);
-	console.log(gameboard);
+	//console.log(gameboard);
 
 	clicker++;			//POCITADLO KLIKOV
 
@@ -135,15 +148,60 @@ function match(event) {
 	}
 
 	if ((countcardT + countcardF) == 2) {    //AK NA DRUHY KLIK NASIEL DVE ROZDIELNE KARTY Z ROVNAKEHO PARU TAK NASIEL CELY PAR
-		points++;
-		let swap=document.getElementById(memoryid);
-		let newElement=document.createElement("IMG");
-		newElement.setAttribute("src", swap.src);
-		newElement.setAttribute("class", "resultElement");
-		document.getElementById("placeForCard").appendChild(newElement);
+
+		if(playerOne.turn==true){			//ten hrac ktory teraz hra a sa nasiel par dostane bod
+			alert("point to "+playerOne.name);
+
+			let swap=document.getElementById(memoryid);
+
+			let N=document.createElement("TR");
+			N.setAttribute("id","mytr"+playerOne.point);
+			document.getElementById("table1N").appendChild(N);
+
+			let y = document.createElement("TD");
+			let t = document.createElement("IMG");
+			t.setAttribute("src", swap.src);
+			y.appendChild(t);
+			document.getElementById("mytr"+playerOne.point).appendChild(y);
+
+			playerOne.point++;
+		}else{
+			alert("point to "+playerTwo.name);
+
+			let swap2=document.getElementById(memoryid);
+
+			let N2=document.createElement("TR");
+			N2.setAttribute("id","mytr2"+playerTwo.point);
+			document.getElementById("table2N").appendChild(N2);
+
+			let y2 = document.createElement("TD");
+			let t2 = document.createElement("IMG");
+			t2.setAttribute("src", swap2.src);
+			y2.appendChild(t2);
+			document.getElementById("mytr2"+playerTwo.point).appendChild(y2);
+
+			playerTwo.point++;
+		}
+
+		playerOne.turn=!playerOne.turn;		//obrati znova false a true aby ked to pojde resetVars zostal hrat ten ktory nasiel par
+		playerTwo.turn=!playerTwo.turn;
+
+
+		//let swap=document.getElementById(memoryid);
+		//let newElement=document.createElement("IMG");
+		//newElement.setAttribute("src", swap.src);
+		//newElement.setAttribute("class", "resultElement");
+		//document.getElementById("placeForCard").appendChild(newElement);
+
 		document.getElementById(memoryid).style.visibility='hidden';
 		document.getElementById(event.target.id).style.visibility='hidden';
-		if(points==(sizeX*sizeY)/2){
+
+		if((playerOne.point+playerTwo.point)==(sizeX*sizeY)/2){
+			if(playerOne.point>playerTwo.point){
+				winner=playerOne.name;
+			}else{
+				winner=playerTwo.name;
+			}
 			end();
 		}
 
@@ -155,7 +213,9 @@ function end(){     //FUNKCIA PO NAJDENI VSETKYCH PAROV
 	clearBoard();
 	document.getElementById("endGame").innerHTML="Gameover";
 	document.getElementById("placeForCard").innerHTML="";
-	points=0;
+	playerOne.point=0;
+	playerTwo.point=0;
+	alert("winner is "+winner);
 	enableRestart();
 
 }
@@ -175,3 +235,66 @@ function restart() {
 
 //document.getElementById("card1").src = "obr/0.jpg";
 
+function start() {				//pri nacitani stranky
+	shuffle();
+	button = document.getElementById('btnP');
+	button.addEventListener("click", event => {			//buttonu na posielanie mien prida akoby funkciu pri kliknuti na neho
+   
+    	names();											//ziska obsah z inputov
+			if(playerOne.name === "" || playerTwo.name === ""){		//ak su inputy prazdne 
+				alert("empty names folders");
+			}else{											//ak v nich je nieco
+
+				create();									
+				disableRestart();
+				applieNames();
+				document.getElementById("endGame").innerHTML="";
+			}
+
+    });
+
+}
+
+let button;
+
+function names(){
+	playerOne.name=document.getElementById("P1").value;
+	playerTwo.name=document.getElementById("P2").value;
+}
+
+function applieNames(){														//zapise mena do tabulky
+		createNameTables();
+		let remove = document.getElementById("inputNames");					//vymaze cele divko
+		remove.parentNode.removeChild(remove);
+
+}
+
+function createNameTables(){
+
+	let table1N=document.createElement("TABLE");
+	table1N.setAttribute("id","table1N");
+	document.body.appendChild(table1N);
+
+	let N=document.createElement("TR");
+	N.setAttribute("id","mytr");
+	document.getElementById("table1N").appendChild(N);
+
+	let y = document.createElement("TD");
+	let t = document.createTextNode(playerOne.name);
+	y.appendChild(t);
+	document.getElementById("mytr").appendChild(y);
+
+	let table2N=document.createElement("TABLE");
+	table2N.setAttribute("id","table2N");
+	document.body.appendChild(table2N);
+
+	let N2=document.createElement("TR");
+	N2.setAttribute("id","mytr2");
+	document.getElementById("table2N").appendChild(N2);
+
+	let y2 = document.createElement("TD");
+	let t2 = document.createTextNode(playerTwo.name);
+	y2.appendChild(t2);
+	document.getElementById("mytr2").appendChild(y2);
+	
+}
